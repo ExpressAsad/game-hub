bf2e2018f7ecdaa9af13e54b0e9000ab9e60fa72
Asad Riaz <asadmernstack@gmail.com>
2026-09-09 17:28:05 +0100

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 const App = () => {
   return (
     <div className="min-h-screen grid grid-rows-[64px_1fr]">
-
       {/* Navbar */}
       <nav className="bg-gray-800"></nav>
 
       {/* Content */}
       <div className="grid grid-cols-1 md:grid-cols-[220px_1fr]">
-
         {/* Sidebar */}
         <aside className="hidden md:block bg-gray-100"></aside>
 
         {/* Main */}
         <main className="bg-white min-h-[500px]"></main>
-
       </div>
-
     </div>
   );
 };
