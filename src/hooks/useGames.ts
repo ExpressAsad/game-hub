c2cc084c654599006d70f2335b92245f3492d5b3
Asad Riaz <asadmernstack@gmail.com>
2026-09-13 11:48:54 +0100

    Fixed unused useQuery hook issue

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query"
-import apiClient from "../services/api-client"
+import { useInfiniteQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
 import type { Genre } from "./useGenres";
 import type { Platform } from "./usePlatforms";
 export interface Game{
